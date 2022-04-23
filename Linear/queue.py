1fccae9247f1bc97e03cf93fa0fbc92b47c2c566
MpumeleloNtobi <ntobimpumelelo@gmail.com>
2022-04-23 15:45:14 +0200

    Update the queue data structure script to be able to handling errors associated with calling the class methods to an empty queue

--- Linear/queue.py
+++ Linear/queue.py
@@ -1,6 +1,6 @@
 class Queue:
     # A class to create a queue object. 
-    # Uses a FIFO policy
+    # Uses a FIFO queuing policy
     def __init__(self):
         self.queue = []
 
@@ -10,8 +10,20 @@ class Queue:
 
     # Remove an element in the front of the queue
     def dequeue(self):
-        self.queue.pop()
+        if len(self.queue) != 0:
+            self.queue.pop()
+
+    # Peek to the front of the queue
+    def front():
+        if len(self.queue) != 0:
+            print(self.queue[-1])
+
+    # Peek to the left of the queue
+    def rear():
+        if len(self.queue) != 0:
+            print(self.queue[0])
 
     # Print out the queue 
     def print_queue(self):
-        " -> ".join(self.queue)
+        if len(self.queue) != 0:
+            " -> ".join(self.queue)
