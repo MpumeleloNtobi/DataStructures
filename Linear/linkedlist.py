70a4c2144ec964d8973bcebad596dd2c14471dbe
MpumeleloNtobi <ntobimpumelelo@gmail.com>
2022-04-23 15:09:39 +0200

    Push version 1 of linked list removed most comments and added docstrings instead

--- Linear/linkedlist.py
+++ Linear/linkedlist.py
@@ -1,23 +1,57 @@
 # Program to implement a linked list data structure from scratch using object-oriented programming
 
-# A class for creating a linked list node object 
 class Node:
+    """
+        A class for creating a node object
+
+        Attributes:
+        ----------
+            data: any data type
+                value of a node
+            next: 
+                pointer to the next node
+
+        Methods:
+        -------
+            None
+    """
     def __init__(self, data):
         self.data = data 
         self.next = None
 
-# A class for creating a linked list object
 class LinkedList:
+    """
+        A class for creating a linked list object
+
+        Attributes:
+        ----------
+            None
+
+        Methods:
+        -------
+            add_first(data)
+                add an element to the beginning of the list
+            add_last(data)
+                add an element to the end of the list
+            remove_first(data)
+                remove an element to the beginning of the list
+            remove_last(data)
+                remove an element to the end of the list
+            rear()
+                return the element at the end of the list
+            front()
+                return the element at the beginning oof the list i.e. the root element
+            print_list()
+                traverse and print all the elements of the list in order
+    """
     def __init__(self):
         self.root = None
 
-    # Add an element to the beginning of the list
     def add_first(self, data):
         new_node = Node(data)
         new_node.next = self.root
         self.root = new_node
 
-    # Add an element to the end of the list
     def add_last(self, data):
         new_node = Node(data)
         if self.root is None:
@@ -28,14 +62,12 @@ class LinkedList:
                 current_node = current_node.next
             current_node.next = new_node
 
-    # Remove an element to the beginning of the list
     def remove_first(self, data):
         if self.root is None:
             pass
         else:
             self.root = self.root.next
 
-    # Remove an element to the end of the list
     def remove_last(self, data):
         if self.root is None:
             pass
@@ -45,14 +77,12 @@ class LinkedList:
                 current_node = current_node.next
             current_node.next = None
         
-    # Return the element at the end of the list
     def rear():
         if self.root is not None:
             print(self.root.data)
         else:
             print(self.root)
 
-    # Return the element at the beginning oof the list i.e. the root element
     def front():
         if self.root is None:
             print(self.root)
